e70fe90dbca724247e4bc4e1b51d022eb8abbd26
Favour Olukayode <nerosilva522@gmail.com>
2021-07-09 21:42:53 +0100

    Genesis

---
+++ android/app/src/main/kotlin/com/example/tauria_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tauria_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+mixin AppColors {
+  static final Color redDark = Color.fromRGBO(245, 49, 63, 1);
+  static final Color redLight = Color.fromRGBO(255, 163, 96, 1);
+  static final Color green = Color.fromRGBO(87, 193, 104, 1);
+  static final Color lightGreen = Color.fromRGBO(222, 243, 225, 1);
+  static final Color textColor = Color.fromRGBO(109, 110, 156, 1);
+  
+	
+}
